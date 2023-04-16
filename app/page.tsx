f53359e6288cb3f60cb14e2fc6088052f66906b1
sambeet kumar khatua <khatuasambeet31@gmail.com>
2023-04-17 02:11:01 +0530

    project completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,76 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { BsClipboardPlus } from "react-icons/bs";
-
-const inter = Inter({ subsets: ["latin"] });
+"use client";
+import { BsClipboardPlus, BsClipboardCheck } from "react-icons/bs";
+import React, { useState } from "react";
+import {
+  numbers,
+  upperCaseLetters,
+  lowerCaseLetters,
+  specialCharacters,
+} from "@/character";
 
 export default function Home() {
+  const [password, setPassword] = useState<any>("");
+  const [passwordLen, setPasswordLen] = useState<string>("4");
+  const [includeUppercase, setIncludeUppercase] = useState<boolean>(false);
+  const [includeLowercase, setIncludeLowercase] = useState<boolean>(false);
+  const [includeNumbers, setIncludeNumbers] = useState<boolean>(false);
+  const [includeSymbols, setIncludeSymbols] = useState<boolean>(false);
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+
+  const handleGenerate = (e: any): void => {
+    e.preventDefault();
+    if (
+      !includeUppercase &&
+      !includeLowercase &&
+      !includeNumbers &&
+      !includeSymbols
+    ) {
+      alert("You must select atleast one option");
+    }
+    let characterList: string = "";
+    if (includeUppercase) {
+      characterList = characterList + upperCaseLetters;
+    }
+    if (includeLowercase) {
+      characterList = characterList + lowerCaseLetters;
+    }
+    if (includeNumbers) {
+      characterList = characterList + numbers;
+    }
+    if (includeSymbols) {
+      characterList = characterList + specialCharacters;
+    }
+
+    setPassword(createPassword(characterList));
+  };
+  const createPassword = (characterlist: string): string => {
+    let password = "";
+    const characterListLength: number = characterlist.length;
+    for (let i: number = 0; i < parseInt(passwordLen); i++) {
+      const characterIndex = Math.round(Math.random() * characterListLength);
+      password = password + characterlist.charAt(characterIndex);
+    }
+    return password;
+  };
+  const handleCopy = (): void => {
+    if (password === "") alert("Nothing to copy");
+    else copyToClipboard(password);
+  };
+  const copyToClipboard = (message: string): void => {
+    setIsCopied(true);
+    navigator.clipboard.writeText(message).then(
+      () => {
+        console.log("copied to clipboard");
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 2000);
+  };
+
   return (
     <section className="min-h-screen flex items-stretch text-white ">
       <div
@@ -42,9 +108,15 @@ export default function Home() {
             Password Generator 💻
           </h1>
           <div className="rounded-lg relative flex items-center justify-center bg-gray-800 py-6 px-4 text-white w-[80%] sm:w-[65%] mx-auto h-10 my-auto">
-            <h3 className="p-2 text-xl font-mono tracking-wide">Password</h3>
-            <button className="tooltip tooltip-bottom text-2xl rounded-lg absolute bg-gray-950 text-white border-none p-2 cursor-pointer top-[10%] right-[3px]" data-tip="copy">
-                <BsClipboardPlus />
+            <h3 className="p-2 text-xl font-mono tracking-wide">{password}</h3>
+            <button
+              className={`tooltip ${
+                isCopied && "tooltip-open"
+              } tooltip-bottom text-2xl rounded-lg absolute bg-gray-950 text-white border-none p-2 cursor-pointer top-[10%] right-[3px]`}
+              data-tip={isCopied ? "Copied" : "copy"}
+              onClick={handleCopy}
+            >
+              {isCopied ? <BsClipboardCheck /> : <BsClipboardPlus />}
             </button>
           </div>
           <form action="" className="sm:w-2/3 w-full px-4 lg:px-0 mx-auto">
@@ -54,11 +126,13 @@ export default function Home() {
                   Password length
                 </p>
                 <input
+                  defaultValue={passwordLen}
                   type="range"
                   min="0"
                   max="8"
                   className="range range-primary"
                   step="1"
+                  onChange={(e) => setPasswordLen(e.target.value)}
                 />
                 <div className="w-full flex justify-between text-xs px-2">
                   <span>0</span>
@@ -73,28 +147,52 @@ export default function Home() {
                 </div>
               </div>
               <label className="relative inline-flex items-center mr-5 cursor-pointer">
-                <input type="checkbox" value="" className="sr-only peer" />
+                <input
+                  type="checkbox"
+                  value=""
+                  className="sr-only peer"
+                  checked={includeUppercase}
+                  onChange={(e) => setIncludeUppercase(e.target.checked)}
+                />
                 <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-2 peer-focus:ring-violet-400 dark:peer-focus:ring-violet-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-violet-600"></div>
                 <span className="ml-3 text-sm font-semibold text-gray-700 dark:text-gray-300">
                   Include Uppercase Letters
                 </span>
               </label>
               <label className="relative inline-flex items-center mr-5 cursor-pointer">
-                <input type="checkbox" value="" className="sr-only peer" />
+                <input
+                  type="checkbox"
+                  value=""
+                  className="sr-only peer"
+                  checked={includeLowercase}
+                  onChange={(e) => setIncludeLowercase(e.target.checked)}
+                />
                 <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-2 peer-focus:ring-violet-400 dark:peer-focus:ring-violet-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-violet-600"></div>
                 <span className="ml-3 text-sm font-semibold text-gray-900 dark:text-gray-300">
                   Include Lowercase Letters
                 </span>
               </label>
               <label className="relative inline-flex items-center mr-5 cursor-pointer">
-                <input type="checkbox" value="" className="sr-only peer" />
+                <input
+                  type="checkbox"
+                  value=""
+                  className="sr-only peer"
+                  checked={includeNumbers}
+                  onChange={(e) => setIncludeNumbers(e.target.checked)}
+                />
                 <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-2 peer-focus:ring-violet-400 dark:peer-focus:ring-violet-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-violet-600"></div>
                 <span className="ml-3 text-sm font-semibold text-gray-900 dark:text-gray-300">
                   Include Numbers
                 </span>
               </label>
               <label className="relative inline-flex items-center mr-5 cursor-pointer">
-                <input type="checkbox" value="" className="sr-only peer" />
+                <input
+                  type="checkbox"
+                  value=""
+                  className="sr-only peer"
+                  checked={includeSymbols}
+                  onChange={(e) => setIncludeSymbols(e.target.checked)}
+                />
                 <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-2 peer-focus:ring-violet-400 dark:peer-focus:ring-violet-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-violet-600"></div>
                 <span className="ml-3 text-sm font-semibold text-gray-900 dark:text-gray-300">
                   Include Symbols
@@ -102,7 +200,10 @@ export default function Home() {
               </label>
             </div>
             <div className="px-4 pb-2 pt-4">
-              <button className="tracking-wider block w-[80%] mx-auto p-2 text-lg font-semibold rounded-full bg-violet-700 hover:bg-violet-800 focus:outline-none duration-150">
+              <button
+                className="tracking-wider block w-[80%] mx-auto p-2 text-lg font-semibold rounded-full bg-violet-700 hover:bg-violet-800 focus:outline-none duration-150"
+                onClick={handleGenerate}
+              >
                 Generate
               </button>
             </div>
